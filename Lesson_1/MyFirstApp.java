5414e8aa5b3929d32818d760e84af99413981e3d
Bystrov Daniil <bstrdn@gmail.com>
2020-02-07 13:24:31 +0300

    add variable

--- Lesson_1/MyFirstApp.java
+++ Lesson_1/MyFirstApp.java
@@ -1,6 +1,5 @@
 public class MyFirstApp {
 	public static void main(String[] args) {
-		System.out.print("HelloWorld!");
-		System.exit(-1);
+		System.out.print("Hello World!");
 	}
 }
---
+++ Lesson_1/Variable.java
@@ -0,0 +1,14 @@
+public class Variable {
+	public static void main(String[] args) {
+		byte cores = 8;
+		short cpuFrequency = 4000;
+		int ramMB = 64000;
+		long hddKB = 1073741824;
+		float tMin = 13.5f;
+		double tMax = 100.2;
+		char class = 'A';
+		boolean isOn = true;
+
+		
+	}
+}
