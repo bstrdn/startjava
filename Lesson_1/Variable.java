abf9d41e830464d2670eb2228da0bbeb8da6540e
Bystrov Daniil <bstrdn@gmail.com>
2020-02-07 13:48:08 +0300

    1

--- Lesson_1/Variable.java
+++ Lesson_1/Variable.java
@@ -6,9 +6,18 @@ public class Variable {
 		long hddKB = 1073741824;
 		float tMin = 13.5f;
 		double tMax = 100.2;
-		char class = 'A';
+		char classPU = 'A';
 		boolean isOn = true;
 
-		
+		System.out.println("Information of my PC:");
+		System.out.println("1. Amoutn of cores " + cores);
+		System.out.println("2. CPU frequency " + cpuFrequency);
+		System.out.println("3. Amount of memory " + ramMB + "MB");
+		System.out.println("4. Amount of memory on Hard disk " + hddKB + "KB");
+		System.out.println("5. Minimal temperature of CPU " + tMin);
+		System.out.println("6. Maximal temperature of CPU " + tMax);
+		System.out.println("7. Class of power usage - " + classPU);
+		System.out.println("8. Is the computer turned on? - " + isOn);
+
 	}
 }
