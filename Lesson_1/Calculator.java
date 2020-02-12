f8f430fa4df70bfc9cc5ec28e7c1c73a5f2b56dd
Bystrov Daniil <bstrdn@gmail.com>
2020-02-12 11:31:17 +0300

    updt

--- Lesson_1/Calculator.java
+++ Lesson_1/Calculator.java
@@ -1,5 +1,9 @@
 public class Calculator {
-	public static int answer (int first, int second, char sign) {
+	public static void main(String[] args) {
+		System.out.println(calculating(3, 5,'*'));
+	}
+
+	public static int calculating(int first, int second, char sign) {
 		int ans = first;
 		if (sign == '+') {
 			ans = first + second;
@@ -12,12 +16,12 @@ public class Calculator {
 		} else if (sign == '^') {
 			int b = ans;
 			while (second > 1) {
-				ans = ans * b;
+				ans *= b;
 				second--; 
 			}
 		} else if (sign == '%') {
 			ans = first % second;
 		}
-	return ans;
+		return ans;
 	}
 }
--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,19 +1,16 @@
 public class MyFirstGame {
 	public static void main(String[] args) {
-		Secret(32, 45);
+		numbCheck(32, 45);
 	}
 
-	public static void Secret (int secretNumb, int numb) {
+	public static void numbCheck(int secretNumb, int numb) {
 			if (numb == secretNumb) {
 				System.out.println("You win");
-			} else {
-				if (numb < secretNumb) {
+			} else if (numb < secretNumb) {
 					System.out.println("You number is less");
 				} else {
 					System.out.println("You number is greater");
 				}
 			}
 	}
-
-
 }
